d5ce4f5b741edf7cdc19e706877e1bd38ee984d2
ywj <stahywj@163.com>
2016-08-17 21:26:58 +0800

    Initial commit

---
+++ src/Ordinate.java
@@ -0,0 +1,41 @@
+/**
+ * Created by kenneth on 2016/8/17.
+ */
+public class Ordinate {
+    private double row_x;
+    private double row_y;
+    private double col_x;
+    private double col_y;
+
+    public void setRow_x(double row_x) {
+        this.row_x = row_x;
+    }
+
+    public double getRow_x() {
+        return row_x;
+    }
+
+    public void setRow_y(double row_y) {
+        this.row_y = row_y;
+    }
+
+    public double getRow_y() {
+        return row_y;
+    }
+
+    public void setCol_x(double col_x) {
+        this.col_x = col_x;
+    }
+
+    public double getCol_x() {
+        return col_x;
+    }
+
+    public void setCol_y(double col_y) {
+        this.col_y = col_y;
+    }
+
+    public double getCol_y() {
+        return col_y;
+    }
+}
---
+++ src/fun.java
@@ -0,0 +1,57 @@
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by kenneth on 2016/8/17.
+ */
+public class fun {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
+        Ordinate ordinate = new Ordinate();
+        List<Ordinate> list = new ArrayList();
+        while (scan.hasNext()){
+
+//            while (count >= 0){
+            /*    String str = scan.nextLine();
+            System.out.println(str);
+                String[] splits = str.split(" ");
+            System.out.println(splits.length);
+                ordinate.setRow_x(Double.parseDouble(splits[0]));
+                ordinate.setRow_y(Double.parseDouble(splits[1]));
+                ordinate.setCol_x(Double.parseDouble(splits[2]));
+                ordinate.setCol_y(Double.parseDouble(splits[3]));
+                list.add(ordinate);
+                count--;*/
+//            }
+            System.out.println(scan.nextLine());
+        }
+       Ordinate ord = min_max(list);
+        System.out.println(ord.getRow_x()+" "+ord.getRow_y()+" "+ord.getCol_x()+" "+ord.getCol_y());
+    }
+    public static Ordinate min_max(List<Ordinate> list){
+        double min_x = 0;
+        double min_y = 0;
+        double max_x = 0;
+        double max_y = 0;
+        Ordinate ord = new Ordinate();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getRow_x() < min_x && list.get(i).getRow_y() < min_y){
+                min_x = list.get(i).getRow_x();
+                min_y = list.get(i).getRow_y();
+            }
+            if (list.get(i).getCol_x() > max_x && list.get(i).getCol_y() > max_y){
+                max_x = list.get(i).getCol_x();
+                max_y = list.get(i).getCol_y();
+            }
+        }
+        ord.setRow_x(min_x);
+        ord.setRow_y(min_y);
+        ord.setCol_x(max_x);
+        ord.setCol_y(max_y);
+        return ord;
+    }
+}
